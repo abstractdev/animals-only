f9bd6b5c01dd3182ae5c179e4f5283444425ead5
abstractdev <fadcny@gmail.com>
2022-06-05 11:49:24 -0400

    change avatars

--- src/data/animals.ts
+++ src/data/animals.ts
@@ -1,68 +1,58 @@
-export const animals = () => {
-  return [
-    {
-      name: "llama",
-      avatar: "https://assets4.lottiefiles.com/packages/lf20_4ll9qg6q.json",
-    },
-    {
-      name: "turkey",
-      avatar:
-        "https://assets7.lottiefiles.com/private_files/lf30_qmv3phvs.json",
-    },
-    {
-      name: "dog",
-      avatar:
-        "https://assets2.lottiefiles.com/private_files/lf30_h8chtt4u.json",
-    },
-    {
-      name: "squid",
-      avatar: "https://assets2.lottiefiles.com/packages/lf20_fsUTir.json",
-    },
-    {
-      name: "bear",
-      avatar: "https://assets8.lottiefiles.com/temporary_files/kmXAVi.json",
-    },
-    {
-      name: "pigeon",
-      avatar: "https://assets9.lottiefiles.com/packages/lf20_F2Mv1p.json",
-    },
-    {
-      name: "owl",
-      avatar:
-        "https://assets10.lottiefiles.com/private_files/lf30_bin73hsy.json",
-    },
-    {
-      name: "panda",
-      avatar: "https://assets6.lottiefiles.com/packages/lf20_puwecidm.json",
-    },
-    {
-      name: "crocodile",
-      avatar: "https://assets8.lottiefiles.com/packages/lf20_f54ylzjs.json",
-    },
-    {
-      name: "cow",
-      avatar: "https://assets2.lottiefiles.com/packages/lf20_ffqpri3j.json",
-    },
-    {
-      name: "lion",
-      avatar: "https://assets2.lottiefiles.com/packages/lf20_vmollwvl.json",
-    },
-    {
-      name: "penguin",
-      avatar:
-        "https://assets6.lottiefiles.com/private_files/lf30_ll1hdda1.json",
-    },
-    {
-      name: "tiger",
-      avatar: "https://assets3.lottiefiles.com/packages/lf20_lc46h4dr.json",
-    },
-    {
-      name: "cat",
-      avatar: "https://assets10.lottiefiles.com/packages/lf20_4rvjxfox.json",
-    },
-    {
-      name: "dolphin",
-      avatar: "https://assets8.lottiefiles.com/packages/lf20_mdyghykl.json",
-    },
-  ];
-};
+export const animals = [
+  {
+    name: "llama",
+    avatar: "https://assets4.lottiefiles.com/packages/lf20_4ll9qg6q.json",
+  },
+  {
+    name: "turkey",
+    avatar: "https://assets7.lottiefiles.com/private_files/lf30_qmv3phvs.json",
+  },
+  {
+    name: "dog",
+    avatar: "https://assets2.lottiefiles.com/private_files/lf30_h8chtt4u.json",
+  },
+  {
+    name: "squid",
+    avatar: "https://assets2.lottiefiles.com/packages/lf20_fsUTir.json",
+  },
+  {
+    name: "bear",
+    avatar: "https://assets8.lottiefiles.com/temporary_files/kmXAVi.json",
+  },
+  {
+    name: "pigeon",
+    avatar: "https://assets9.lottiefiles.com/packages/lf20_F2Mv1p.json",
+  },
+  {
+    name: "owl",
+    avatar: "https://assets10.lottiefiles.com/private_files/lf30_bin73hsy.json",
+  },
+  {
+    name: "crocodile",
+    avatar: "https://assets8.lottiefiles.com/packages/lf20_f54ylzjs.json",
+  },
+  {
+    name: "lion",
+    avatar: "https://assets2.lottiefiles.com/packages/lf20_vmollwvl.json",
+  },
+  {
+    name: "penguin",
+    avatar: "https://assets6.lottiefiles.com/private_files/lf30_ll1hdda1.json",
+  },
+  {
+    name: "tiger",
+    avatar: "https://assets3.lottiefiles.com/packages/lf20_lc46h4dr.json",
+  },
+  {
+    name: "cat",
+    avatar: "https://assets10.lottiefiles.com/packages/lf20_4rvjxfox.json",
+  },
+  {
+    name: "mouse",
+    avatar: "https://assets1.lottiefiles.com/packages/lf20_9tnkyafk.json",
+  },
+  {
+    name: "toucan",
+    avatar: "https://assets2.lottiefiles.com/private_files/lf30_kcohh4m7.json",
+  },
+];
