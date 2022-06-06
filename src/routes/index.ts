28c4719e8fc899a7d063f239951ce510226ceea3
abstractdev <fadcny@gmail.com>
2022-06-06 09:13:52 -0400

    add status and log-in routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
-const User = require("../models/user");
+import passport from "passport";
 
 const router = express.Router();
 const userController = require("../controllers/userController");
+const statusController = require("../controllers/statusController");
 
 /* GET index. */
 router.get("/", function (req, res) {
@@ -13,7 +14,17 @@ router.get("/sign-up", userController.userCreateGet);
 /* POST sign-up. */
 router.post("/sign-up", userController.userCreatePost);
 /* GET user-page. */
-router.get("/users/:user", function (req, res) {
+router.get("/user/:username", function (req, res) {
   res.render("user");
 });
+/* GET status. */
+router.get("/user/:username/status", statusController.statusCreateGet);
+/* POST status. */
+router.post("/user/:username/status", statusController.statusCreatePost);
+
+//GET log-in
+router.get("/log-in", userController.userLoginGet);
+//POST log-in
+router.post("/log-in", userController.userLoginPost);
+
 export default router;
