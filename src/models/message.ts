401ec6e7c1118ff5a122fc1aeb755ca434ba01d8
abstractdev <fadcny@gmail.com>
2022-06-06 14:25:01 -0400

    change message user id to string

--- src/models/message.ts
+++ src/models/message.ts
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const MessageSchema = new Schema({
-  title: { type: String, required: true, maxLength: 50 },
-  body: { type: String, required: true, maxLength: 256 },
-  postedOn: { type: Date, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-});
+const MessageSchema = new Schema(
+  {
+    title: { type: String, required: true, maxLength: 50 },
+    message: { type: String, required: true, maxLength: 256 },
+    user: { type: String, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
+
+//Export model
+module.exports = mongoose.model("Message", MessageSchema);
