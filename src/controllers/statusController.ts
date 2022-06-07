1d2683f4164f585ef292b357df858adb8fd09a69
abstractdev <fadcny@gmail.com>
2022-06-06 20:28:17 -0400

    add admin status post

--- src/controllers/statusController.ts
+++ src/controllers/statusController.ts
@@ -12,7 +12,7 @@ exports.statusCreateGet = function (
 };
 
 //status POST
-exports.statusCreatePost = [
+exports.statusGuestPost = [
   // Validate and sanitize fields.
   body("secret").trim().escape(),
   check("secret")
@@ -25,7 +25,6 @@ exports.statusCreatePost = [
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      console.log(errors);
       res.render("status", {
         errors: errors.array(),
         post: true,
@@ -34,15 +33,46 @@ exports.statusCreatePost = [
     } else {
       // Secret is correct. Update user status
       const updateUser = (async () => {
-        const filter = { username: req.params.user };
+        const filter = { username: req.params.username };
         const update = { status: "member" };
 
         // `doc` is the document _before_ `update` was applied
         let doc = await User.findOneAndUpdate(filter, update);
 
         //successful - redirect to new user record.
-        console.log("success");
-        res.redirect("/");
+        res.redirect(`/user/${req.params.username}/status/member`);
+      })();
+    }
+  },
+];
+
+exports.statusMemberPost = [
+  // Validate and sanitize fields.
+  body("admin-password").trim().escape(),
+  check("admin-password").isIn(["abstractdev"]).withMessage("Incorrect"),
+
+  // Process request after validation and sanitization.
+  (req: Request, res: Response, next: NextFunction) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("status", {
+        errors: errors.array(),
+        post: true,
+      });
+      return;
+    } else {
+      // Admin password is correct. Update user status
+      const updateUser = (async () => {
+        const filter = { username: req.params.username };
+        const update = { status: "admin" };
+
+        // `doc` is the document _before_ `update` was applied
+        let doc = await User.findOneAndUpdate(filter, update);
+
+        //successful - redirect to new user record.
+        res.redirect(`/user/${req.params.username}/status/member`);
       })();
     }
   },
